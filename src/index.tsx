b639119cbdf6765bb05323a635fed0993ebe9baa
StephenRicketts <stephen.ricketts12@gmail.com>
2021-02-02 21:48:16 -0500

    refactored for multiple code cells, started with making them resizable

--- src/index.tsx
+++ src/index.tsx
@@ -1,73 +1,20 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import * as esbuild from 'esbuild-wasm';
-import { useState, useEffect, useRef } from 'react';
-import ReactDom from 'react-dom';
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
-import { fetchPlugin } from './plugins/fetch-plugin'
-import CodeEditor from './components/code-editor';
-import Preview from './components/preview'
+import ReactDOM from 'react-dom';
+import CodeCell from './components/code-cell';
 
 const App = () => {
-
-  const ref = useRef<any>();
-  const [code, setCode] = useState('');
-  const [input, setInput] = useState('');
-
-  const startService = async () => {
-    ref.current = await esbuild.startService({
-      worker: true,
-      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
-    });
-    
-  };
-
-  const onClick = async () => {
-    if (!ref.current) {
-      return;
-    }
-
-    
-
-    const result = await ref.current.build({
-      entryPoints: ['index.js'],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-      define: {
-        'process.env.NODE_ENV': '"production"',
-        global: 'window'
-      }
-    });
-    
-    setCode(result.outputFiles[0].text);
-    
-
-
-      
-  };
-
-  useEffect(() => {
-    startService();
-  }, []);
-
-
-
+ 
 
   return (
   <div>
-    <CodeEditor 
-    onChange={(value) => setInput(value)} 
-    initialValue="" />
-    <div>
-      <button onClick={onClick}>Submit</button>
-    </div>
-    <Preview code={code} />
+   <CodeCell />
+   <CodeCell />
   </div>
   );
 };
 
 
-ReactDom.render(
+ReactDOM.render(
   <App/>,
   document.querySelector('#root')
 );
