99596ef1225a40fc6a36b24edf3b603e3cad475a
StephenRicketts <stephen.ricketts12@gmail.com>
2021-02-15 21:11:10 -0500

    started with compiling all code cells together

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,3 +1,4 @@
+import './code-cell.css'
 import { useEffect } from 'react';
 import CodeEditor from './code-editor';
 import Preview from './preview';
@@ -13,17 +14,53 @@ interface CodeCellProps {
 const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const { updateCell, createBundle } = useActions();
   const bundle = useTypedSelector((state) => state.bundles[cell.id]);
-  
+  const cumulativeCode = useTypedSelector((state) => {
+    const { data, order } = state.cells;
+    const orderedCells = order.map(id => data[id]);
 
+    const cumulativeCode = [
+      `
+      const show = (value) => {
+        const root = document.querySelector('#root');
+        if (typeof value === 'object) {
+          if(value.$$typeof && value.props) {
+            ReactDOM.render(value, document.querySelector('#root"));
+
+          } else {
+          document.querySelector('#root).innerHTML = JSON.stringify(value);
+        } else {
+          document.querySelector('#root).innerHTML = value;
+        }  
+      };
+      `
+    ];
+    for (let c of orderedCells) {
+      if (c.type === 'code') {
+        cumulativeCode.push(c.content);
+      }
+      if (c.id === cell.id) {
+        break;
+      }
+    }
+    return cumulativeCode;
+  });
+
+  
   useEffect(() => {
+    if (!bundle) {
+    createBundle(cell.id, cumulativeCode.join('\n'));
+    return;
+    }
+
     const timer = setTimeout(async () => {
-      createBundle(cell.id, cell.content);
+      createBundle(cell.id, cumulativeCode.join('\n'));
     }, 750);
 
     return () => {
       clearTimeout(timer);
     };
-  }, [cell.content, cell.id, createBundle]);
+    //eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [cumulativeCode.join('\n'), cell.id, createBundle]);
 
   return (
   <Resizable direction="vertical">
@@ -34,7 +71,17 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
          onChange={(value) => updateCell(cell.id, value)} 
          />
       </Resizable>
-    {bundle && <Preview code={bundle.code} err={bundle.err} />}
+      <div className="progress-wrapper">
+      {
+        !bundle || bundle.loading
+        ? 
+          <div className="progress-cover">
+            <progress className="progress is-small is-primary" max="100">Loading</progress>
+          </div>
+          
+        : <Preview code={bundle.code} err={bundle.err} /> 
+      }
+    </div>
     </div>
   </Resizable>
   );
